cf4bfaa6a2d8c4d9410b6a779f72d0f9c3085099
Tyler Servais <tylerservais@gmail.com>
2019-01-28 09:22:17 -0800

    saves

--- testing_api.py
+++ testing_api.py
@@ -1,9 +1,19 @@
 import numpy as np 
 import pandas as pd 
-import unirest
+import urllib
+import requests
+from pprint import pprint
 
-response = unirest.get("https://skyscanner-skyscanner-flight-search-v1.p.rapidapi.com/apiservices/browsequotes/v1.0/US/USD/en-US/SFO-sky/JFK-sky/2019-01-01?inboundpartialdate=2019-01-01",
+date = '2019-02-01'
+departure_airport = 'ORD'
+final_airport = 'JFK'
+
+query_string = "https://skyscanner-skyscanner-flight-search-v1.p.rapidapi.com/apiservices/browseroutes/v1.0/US/USD/en-US/"+departure_airport+"-sky/"+final_airport+"-sky/"+date+"?inboundpartialdate=2019-02-01"
+
+r = requests.get(query_string,
   headers={
     "X-RapidAPI-Key": "863928ac98msh92375cb362edbadp19fffbjsn3f22e03123fa"
   }
 )
+
+pprint(r.json())
